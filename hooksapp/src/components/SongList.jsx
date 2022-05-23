8f8b0532b42b703c109fae0ec6bf6b39b43a1de0
siam666-shell <abdullahsiam009@gmail.com>
2022-05-23 16:24:24 +0600

    useState with form

--- hooksapp/src/components/SongList.jsx
+++ hooksapp/src/components/SongList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
   const [songs, setSongs] = useState([
@@ -8,8 +9,8 @@ const SongList = () => {
     { title: 'darkside', id: 3 },
   ]);
 
-  const addSong = () => {
-    setSongs([...songs, { title: 'new song', id: uuid() }]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuid() }]);
   };
 
   return (
@@ -19,7 +20,7 @@ const SongList = () => {
           return <li key={song.id}>{song.title}</li>;
         })}
       </ul>
-      <button onClick={addSong}>add a song</button>
+      <NewSongForm addSong={addSong} />
     </div>
   );
 };
