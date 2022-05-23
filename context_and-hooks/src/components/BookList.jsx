8e06c94d994fb8c21f6279dadd5bda3b4e5f2d65
siam666-shell <abdullahsiam009@gmail.com>
2022-05-23 17:19:01 +0600

    context used with functional component

--- context_and-hooks/src/components/BookList.jsx
+++ context_and-hooks/src/components/BookList.jsx
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-export default class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { dark, isLightTheme, light } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>gitanjali</li>
-          <li style={{ background: theme.ui }}>bidrohee</li>
-          <li style={{ background: theme.ui }}>quantam mechanix</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>gitanjali</li>
+        <li style={{ background: theme.ui }}>bidrohee</li>
+        <li style={{ background: theme.ui }}>quantam mechanix</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
