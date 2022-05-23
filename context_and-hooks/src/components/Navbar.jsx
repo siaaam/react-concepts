238df52d5d7654971273e4383447f4a3df05328c
siam666-shell <abdullahsiam009@gmail.com>
2022-05-23 14:56:39 +0600

    navbar accessed data with themecontext.consumer. we can get multiple context data in one component with this approch

--- context_and-hooks/src/components/Navbar.jsx
+++ context_and-hooks/src/components/Navbar.jsx
@@ -1,21 +1,50 @@
+// import React, { Component } from 'react';
+// import { ThemeContext } from '../contexts/ThemeContext';
+
+// export default class Navbar extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     console.log(this.context);
+//     const { dark, isLightTheme, light } = this.context;
+//     const theme = isLightTheme ? light : dark;
+// return (
+//   <nav style={{ background: theme.ui, color: theme.syntax }}>
+//     <h1>Context App</h1>
+//     <ul>
+//       <li>Home</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   </nav>
+// );
+//   }
+// }
+
+// another way of accessing context is ** context.consumer followed by a callback function
+
 import React, { Component } from 'react';
+
 import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    console.log(this.context);
-    const { dark, isLightTheme, light } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, dark, light } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
