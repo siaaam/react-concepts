6cc90a6adafde403c38470dcd9845bb937f90351
siam666-shell <abdullahsiam009@gmail.com>
2022-05-23 15:55:51 +0600

    initialized hooks app and created songlist component

---
+++ hooksapp/src/components/SongList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>this wild darkness</li>
+        <li>lily</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
