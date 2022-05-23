d776ccaa179d68c7942e071a15048dbd9edbdd9d
siam666-shell <abdullahsiam009@gmail.com>
2022-05-23 21:11:37 +0600

    booklist component completed

---
+++ reading-list/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import BookContextProvider from './contexts/BookContext';
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ reading-list/src/components/BookList.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import BookDetails from './BookDetails';
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+  return books.length ? (
+    <>
+      <div className="book-list">
+        <ul>
+          {books.map((book) => (
+            <BookDetails book={book} />
+          ))}
+        </ul>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="empty">books is empty</div>
+    </>
+  );
+};
+
+export default BookList;
